8b61cebeaaf18a386a1cc5d923fe461f450ab8d2
satyam904 <sahusatyam908@gmail.com>
2025-11-27 23:13:27 +0530

    Updated project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+      <About/>
+    </main>
   )
 }
 
